553e928dbf18e098fbc58b3b0677797eaffe42ec
qs0va <110431172+qs0va@users.noreply.github.com>
2023-05-13 22:21:10 +0300

    Add files via upload

--- lab4.cpp
+++ lab4.cpp
@@ -1,243 +1,215 @@
 #include <iostream>
-#include <vector>
+#include <array>
 #include <queue>
-#include <list>
-#include <string>
-#include <bitset>
-#include <climits>
-#include <algorithm>
+
 #include "lab4.h"
-/*Используемые алгоритмы :
-* Second chance
-* Working set
-*/
-//таймер в данной задаче отсчитывает каждую инструкцию доступа к памяти
-class Timer {
-    std::uint64_t counter;
-    std::uint64_t interruptEveryN = 5;
-public:
-    Timer() { counter = 0; }
-    std::uint64_t inc() {
-        if (ULLONG_MAX == ++counter)
-            counter = 0;
-        return counter;
-    }
-    std::uint64_t get() const { return counter; }
-    bool is_interrupt() { return !(counter % interruptEveryN); }
-};
 
-//страница из таблицы страниц
-class Page {
-    //20 бит под номер виртуальной страницы
-    constexpr static int bitsInVPN = 20;
-    //номер виртуальной страницы
-    std::bitset<bitsInVPN> VPN;
-    //биты: обращения, модификации, пустоты - (используется для корректного отображения содержимого страницы)
+#define READ 0
+#define WRITE 1
+#define SIZE 5
 
-public:
-    bool R, M, E;
-    Page() {
-        R = 0;
-        M = 0;
-        E = 1;
-        VPN.reset(); //установка всех битов в 0
-    }
-    void reference() { R = 1; }
-    void modify() { M = 1; R = 1; }
-    bool is_empty() const { return E; }
-    void reset_R() { R = 0; }
-    bool is_equal(std::bitset<bitsInVPN> VPN) const {
-        return (!E && (this->VPN == VPN));
-    }
-    bool set_VPN(std::uint32_t VPN) {
-        std::bitset<bitsInVPN> maxVPN;
-        maxVPN.set(); //установка всех битов в 1
-        if (VPN > maxVPN.to_ulong()) return false;
-        this->VPN = VPN;
-        E = 0;
-        R = 0;
-        M = 0;
-        return true;
-    }
-    std::string get_VPN() const {
-        return E ? "#" : (std::to_string(VPN.to_ulong())
-            //+ "(" + std::to_string(R) + ", " + std::to_string(M) + ')'
-            );
-    }
-
-};
-
-class Memory {
-private:
-    //VPN - virtual page number
-    // PPN - physical page number
-    int find_empty() const {
-        for (int i = 0; i < PTsize; ++i)
-            if (PT[i].is_empty()) return i;
-        return -1;
-    }
-    int find_page(std::uint32_t VPN) const {
-        for (int i = 0; i < PTsize; ++i)
-            if (PT[i].is_equal(VPN)) return i;
-        return -1;
-    }
-    void after_op_action() {
-        tim.inc();
-        if (tim.is_interrupt())
-            for (Page& page : PT)
-                page.reset_R();
-    }
-protected:
-    //вектор страниц, содержащихся таблице страниц (page table)
-    std::vector<Page> PT;
-    const int PTsize = 5;
-    Timer tim;
-
-    Memory() { PT.resize(PTsize); }
-
-    //действия алгоритма при обращении к загруженной странице
-    virtual void founded_page_actions(int PPN) {}
-    //действия алгоритма при загрузке страницы на пустое место
-    virtual void loaded_page_actions(int PPN) {}
-    //действия алгоритма при вытеснении страницы
-    virtual int swapped_page_actions() { return 0; }
-public:
-    unsigned int pageMissCounter = 0;
-    virtual void operation(bool opCode, int VPN) {
-        //поиск страницы в таблице страниц
-        int PPN = find_page(VPN);
-        //если страница не найдена
-        if (PPN == -1) {
-            //поиск пустой страницы
-            PPN = find_empty();
-            //если пустых страниц нет
-            if (PPN == -1)
-                PPN = swapped_page_actions();
-            else loaded_page_actions(PPN);
-            //поместить новую страницу 
-            PT[PPN].set_VPN(VPN);
-            ++pageMissCounter; //для анализа кол-во ошибок отсутствия страницы (отладочная информация)
-        }
-        else founded_page_actions(PPN);
-        //произвести операцию
-        opCode ? PT[PPN].modify() : PT[PPN].reference();
-        after_op_action();
-    }
-
-    virtual std::string get_PT() const {
-        std::string str;
-        for (Page page : PT)
-            str += (page.get_VPN() + ' ');
-        str.pop_back();
-        return str;
-    };
-};
-
-class MemorySecondChance : public Memory {
-private:
-    std::queue<int> pagesNums;
-    int swapped_page_actions() override {
-        int pageNum;
-        bool exit = false;
-        /*Взять страницу из очереди, если R = 0 (pop)
-        * иначе - сбросить R и поместить в конец (reset, pop, push)
-        */
-        while (!exit) {
-            pageNum = pagesNums.front();
-            if (PT[pageNum].R)
-                PT[pageNum].reset_R();
-            else exit = true;
-            pagesNums.push(pageNum);
-            pagesNums.pop();
-        };
-        return pageNum;
-    }
-
-    void loaded_page_actions(int PPN) override { pagesNums.push(PPN); };
-public:
-    MemorySecondChance() : Memory() {};
-};
-
-
-class MemoryWorkingSet : public Memory {
-private:
-    std::vector<std::uint64_t> pagesTime;
-
-    virtual void loaded_page_actions(int PPN) {
-        //установка текущего времени для всех страниц с R=1
-        for (int i = 0; i < PTsize; ++i)
-            if (PT[i].R && !PT[i].E)
-                pagesTime[i] = tim.get();
-        pagesTime[PPN] = tim.get();
-    }
-
-    virtual int swapped_page_actions() {
-        //установка текущего времени для всех страниц с R=1
-        for (int i = 0; i < PTsize; ++i)
-            if (PT[i].R)
-                pagesTime[i] = tim.get();
-
-        //расчет возраста каждой страницы
-        std::vector<std::uint64_t> pagesAge(PTsize);
-        for (int i = 0; i < PTsize; ++i)
-            pagesAge[i] = tim.get() - pagesTime[i];
-
-        //выборка страниц с максимальным возрастом
-        std::vector<std::uint64_t>::iterator maxAgeIt = std::max_element(pagesAge.begin(),
-            pagesAge.end());
-        std::vector<std::uint64_t> maxAgePages;
-        maxAgePages.reserve(PTsize);
-        for (int i = 0; i < PTsize; ++i)
-            if (pagesAge[i] == *maxAgeIt)
-                maxAgePages.push_back(i);
-
-        int displacePageNum;
-        //если страница с максимальным возрастом не одна
-        if (maxAgePages.size() != 1) {
-            //выбрать только не модифицированные
-            std::vector<std::uint64_t> notModifiedPages;
-            notModifiedPages.reserve(maxAgePages.size());
-            for (int i = 0; i < maxAgePages.size(); ++i)
-                if (!PT[maxAgePages[i]].M)
-                    notModifiedPages.push_back(maxAgePages[i]);
-
-            //если есть не модифицированные страницы -> случайно вернуть одну из них
-            if (notModifiedPages.size() != 0)
-                displacePageNum = notModifiedPages[uniform_rnd(0, notModifiedPages.size() - 1)];
-            //если есть нет не модифицированных страниц -> случайно вернуть одну из самых старых
-            else
-                displacePageNum = maxAgePages[uniform_rnd(0, maxAgePages.size() - 1)];
-        }
-        else displacePageNum = maxAgePages[0];
-
-        //присвоить новой странице текущее время
-        pagesTime[displacePageNum] = tim.get();
-        return displacePageNum;
-    }
-
-public:
-    MemoryWorkingSet() : Memory() { pagesTime.resize(PTsize); }
-};
-
-void do_operations(Memory* mem) {
-    bool opCode;
+struct PhysicalPage {
     int VPN;
-    while (std::cin >> opCode >> VPN) {
-        (*mem).operation(opCode, VPN);
-        std::cout << (*mem).get_PT() << std::endl;
-        //std::cout << (*mem).pageMissCounter << std::endl;
+    bool R;
+    bool M;
+    int que;
+    unsigned int counter;
+};
+
+struct Command {
+    int type;
+    int VPN;
+};
+
+typedef std::array<PhysicalPage, SIZE> Table;
+
+void run(Table::iterator(*alg) (Table&));
+
+void print(const Table& table);
+void dprint(const Table& table);
+
+void insert(Table& table, const Table::iterator& iter, int VPN);
+
+void doReset(Table& table);
+
+Table::iterator find(Table& table, int VPN);
+
+Table::iterator getBlockToReplace(Table& table, Table::iterator(*alg) (Table&));
+Table::iterator getEmpty(Table& table);
+
+Table::iterator FIFO(Table& table);
+Table::iterator WS(Table& table);
+
+unsigned int systemTime = 0;
+
+int main(int argc, char *argv[]) {
+    //if (argc > 1) {
+    //    if (strcmp("1", argv[1]) == 0) {
+    //        secondChance();
+    //    }
+    //    else if (strcmp("2", argv[1]) == 0) {
+    //        NFU();
+    //    }
+    //    else {
+    //        std::cout << "Wrong input\n";
+    //    }
+    //}
+    //else {
+    //    std::cout << "No input\n";
+    //}
+    run(WS);
+    return 0;
+}
+
+void run(Table::iterator(*alg) (Table&)) {
+    Table table;
+    PhysicalPage temp;
+    temp.VPN = -1;
+    temp.R = false;
+    temp.M = false;
+    temp.que = -1;
+    temp.counter = 0;
+    for (auto it = table.begin(); it < table.end(); it++) {
+        *it = temp;
+    }
+
+    Command command;
+    int resetCounter = 0;
+    while (std::cin >> command.type >> command.VPN) {
+
+        Table::iterator block = find(table, command.VPN);
+        if (block == table.end()) {
+            for (auto it = table.begin(); it < table.end(); it++) {
+                if (it->R) {
+                    it->counter = systemTime;
+                }
+            }
+
+            block = getBlockToReplace(table, alg);
+            insert(table, block, command.VPN);
+            block->M = false;
+            block->counter = systemTime;
+        }
+
+        block->R = true;
+        if (command.type == WRITE) {
+            block->M = true;
+        }
+
+        dprint(table);
+
+        resetCounter++;
+        if (resetCounter == 5) {
+            doReset(table);
+            std::cout << "reset\n";
+            resetCounter = 0;
+        }
+        systemTime++;
     }
 }
 
-int main(int argc, char* argv[])
-{
-    int alg = 1;
-    Memory* memory;
-    if (argc > 1)
-        alg = std::atoi(argv[1]);
-    if (alg == 1) memory = new MemorySecondChance();
-    else memory = new MemoryWorkingSet();
-    do_operations(memory);
-    delete memory;
-    return 0;
+void print(const Table& table) {
+    for (auto it = table.begin(); it < table.end(); it++) {
+        if (it->VPN == -1) {
+            std::cout << '#';
+        }
+        else {
+            std::cout << it->VPN;
+        }
+        if (it + 1 != table.end()) std::cout << ' ';
+    }
+    std::cout << '\n';
+}
+
+void dprint(const Table& table) {
+    for (auto p : table) {
+        if (p.VPN == -1) {
+            std::cout << '#';
+        }
+        else {
+            std::cout << p.VPN;
+        }
+        std::cout << '(' << (int)p.R << ',' << (int)p.M << ',' << p.counter << ") ";
+    }
+    std::cout << "\n\n";
+}
+
+Table::iterator find(Table& table, int VPN) {
+    for (auto it = table.begin(); it < table.end(); it++) {
+        if (it->VPN == VPN) return it;
+    }
+    return table.end();
+}
+
+void doReset(Table& table) {
+    for (auto it = table.begin(); it < table.end(); it++) {
+        it->R = false;
+    }
+}
+
+Table::iterator getBlockToReplace(Table& table, Table::iterator (*alg) (Table&)) {
+    auto block = getEmpty(table);
+    if (block != table.end()) return block;
+    return alg(table);
+}
+
+Table::iterator getEmpty(Table& table) {
+    for (auto it = table.begin(); it < table.end(); it++) {
+        if (it->VPN == -1) return it;
+    }
+    return table.end();
+}
+
+
+Table::iterator FIFO(Table& table) {
+    Table::iterator iter;
+    for (auto it = table.begin(); it < table.end(); it++) {
+        if (it->que == 0) {
+            iter = it;
+            break;
+        }
+    }
+    return iter;
+}
+
+Table::iterator WS(Table& table) {
+    unsigned int maxAge = 0;
+    for (auto it = table.begin(); it < table.end(); it++) {
+        if (systemTime - it->counter > maxAge) {
+            maxAge = systemTime - it->counter;
+        }
+    }
+
+    std::vector<PhysicalPage> subList;
+    for (auto it = table.begin(); it < table.end(); it++) {
+        if (systemTime - it->counter == maxAge) {
+            subList.push_back(*it);
+        }
+    }
+
+    for (int i = 0; i < subList.size(); i++) {
+        if (subList.at(i).M) {
+            subList.erase(subList.begin() + i);
+            i--;
+        }
+    }
+
+    int VPNToReplace;
+    if (!subList.empty()) {
+        VPNToReplace = subList.at(uniform_rnd(0, subList.size() - 1)).VPN;
+    }
+    else {
+        VPNToReplace = table.at(uniform_rnd(0, SIZE - 1)).VPN;
+    }
+
+    return find(table, VPNToReplace);
+}
+
+
+void insert(Table& table, const Table::iterator& iter, int VPN) {
+    for (auto it = table.begin(); it < table.end(); it++) {
+        if (it->que != -1) it->que--;
+    }
+    iter->VPN = VPN;
+    iter->que = SIZE - 1;
 }
